03d212f53678ca5fbb2f7695947fdfd0342e3de4
thebestkyle323 <115924430+thebestkyle323@users.noreply.github.com>
2024-08-23 20:52:13 +0800

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const bot = new Telegraf(TOKEN);
 
 async function sendTgMessage() {
   const htmlContent = `
-    <b>苹果全权限UDID证书</b><a href="https://docs.qq.com/doc/DRlByYkFobFdGTE9Z">教程合集</a>\n
+    <b>苹果全权限UDID证书</b> (<a href="https://docs.qq.com/doc/DRlByYkFobFdGTE9Z">教程合集</a>)\n
     
     证书支持：\n
     官微和多开微信无后台消息推送\n
@@ -20,11 +20,8 @@ async function sendTgMessage() {
     质保1年:110，质保半年:90，质保30天:50，无质保:35\n
     预约证书（一般72小时出证书）\n
     质保1年:60，质保30天:30，无质保:15\n
-    
-    爱玩小店：<a href="https://m.iwanshare.club">证书购买</a>\n
-    证书签名：<a href="https://udid.iwanshare.club">点击前往</a>\n
-
     <b>限时特惠，凡购买110和60年质保用户均送ipad证书一个</b>
+    <a href="https://m.iwanshare.club">证书购买</a> | <a href="https://udid.iwanshare.club">证书签名</a> | <a href="https://vip.iosfans.club">iOS社区</a>\n
   `;
 
   try {
